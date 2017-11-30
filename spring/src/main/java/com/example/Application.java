0ae3e01040c23a857b54130b10db53ca3d83c34d
Thomas Qvarnström <tqvarnst@redhat.com>
2017-11-30 15:33:06 +0100

    Small fixes

--- spring/src/main/java/com/example/Application.java
+++ spring/src/main/java/com/example/Application.java
@@ -14,16 +14,16 @@
  * limitations under the License.
  */
 
-package io.openshift.booster;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoosterApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BoosterApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
