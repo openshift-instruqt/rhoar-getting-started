f4908a20d300ebdd3eec9bf142582c37fa47a32b
Eric Deandrea <eric.deandrea@gmail.com>
2021-06-17 14:30:48 -0400

    Update Spring Data JPA example (#61)

--- spring/spring-db-access/src/main/java/com/example/service/FruitController.java
+++ spring/spring-db-access/src/main/java/com/example/service/FruitController.java
@@ -1,53 +1,54 @@
 package com.example.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping(value = "/api/fruits")
 public class FruitController {
+    private final FruitRepository repository;
 
-    private FruitRepository repository;
-
-    @Autowired
     public FruitController(FruitRepository repository) {
         this.repository = repository;
     }
 
     @GetMapping
     @ResponseBody
-    public List<Fruit> getAll() {
-        return StreamSupport.stream(repository.findAll().spliterator(), false).collect(Collectors.toList());
+    public Iterable<Fruit> getAll() {
+        return this.repository.findAll();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public String createFruit(@RequestBody(required = false) Fruit fruit) {
-        repository.save(fruit);
+        this.repository.save(fruit);
         return "redirect:/";
     }
 
     @GetMapping("/{id}")
     public Fruit getFruit(@PathVariable("id") Long id) {
-        return repository.findById(id).orElse(null);
+        return this.repository.findById(id).orElse(null);
     }
 
     @PutMapping("/{id}")
     public Fruit updateFruit(@PathVariable("id") Long id, @RequestBody(required = false) Fruit fruit) {
         fruit.setId(id);
-        return repository.save(fruit);
+        return this.repository.save(fruit);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Long id) {
-        repository.deleteById(id);
+        this.repository.deleteById(id);
     }
 
 }
