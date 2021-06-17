5bf9efec3580de11c6b516d21d681adff2ec2f39
Eric Deandrea <eric.deandrea@gmail.com>
2021-06-17 14:31:10 -0400

    Update Spring Data JPA example (#62)
    Goes along with openshift-labs/learn-katacoda#1193 and #61

--- spring/spring-db-access/src/test/java/com/example/service/DatabaseTest.java
+++ spring/spring-db-access/src/test/java/com/example/service/DatabaseTest.java
@@ -1,56 +1,79 @@
 package com.example.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("local")
+@Transactional
 public class DatabaseTest {
-
     @Autowired
     private FruitRepository fruitRepository;
 
     @Test
     public void testGetAll() {
-        assertTrue(fruitRepository.findAll().spliterator().getExactSizeIfKnown() == 3);
+        assertThat(this.fruitRepository.findAll())
+          .hasSize(3);
     }
 
     @Test
     public void getOne() {
-        assertNotNull(fruitRepository.findById(1L).orElse(null));
+        assertThat(this.fruitRepository.findById(1L))
+          .isNotNull()
+          .isPresent();
     }
 
     @Test
     public void updateAFruit() {
-        Fruit apple = fruitRepository.findById(2L).orElse(null);
-        assertNotNull(apple);
-        assertEquals("Apple", apple.getName());
+        Optional<Fruit> appleOptional = this.fruitRepository.findById(2L);
 
+        assertThat(appleOptional)
+          .isNotNull()
+          .isPresent()
+          .get()
+          .extracting(Fruit::getName)
+          .isEqualTo("Apple");
+
+        Fruit apple = appleOptional.get();
         apple.setName("Green Apple");
-        fruitRepository.save(apple);
+        this.fruitRepository.save(apple);
 
-        assertEquals("Green Apple", fruitRepository.findById(2L).orElse(null).getName());
+        assertThat(this.fruitRepository.findById(2L))
+          .isNotNull()
+          .isPresent()
+          .get()
+          .extracting(Fruit::getName)
+          .isEqualTo("Green Apple");
     }
 
     @Test
     public void createAndDeleteAFruit() {
-        Long orangeId = fruitRepository.save(new Fruit("Orange")).getId();
-        Fruit orange = fruitRepository.findById(orangeId).orElse(null);
-        assertNotNull(orange);
+        Long orangeId = this.fruitRepository.save(new Fruit("Orange")).getId();
+        Optional<Fruit> orange = this.fruitRepository.findById(orangeId);
 
-        fruitRepository.deleteById(orangeId);
-        assertNull(fruitRepository.findById(orangeId).orElse(null));
+        assertThat(orange)
+          .isNotNull()
+          .isPresent();
+
+        this.fruitRepository.deleteById(orangeId);
+
+        assertThat(this.fruitRepository.findById(orangeId))
+          .isNotNull()
+          .isNotPresent();
     }
 
     @Test
     public void getWrongId() {
-        assertNull(fruitRepository.findById(9999L).orElse(null));
+        assertThat(this.fruitRepository.findById(9999L))
+          .isNotNull()
+          .isNotPresent();
     }
 }
