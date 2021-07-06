4d0e8d2a61fca1ac1b0650b24c194ce2ebcf3bce
Dewan Ahmed <dewan.ishtiaque@hotmail.com>
2021-07-06 16:06:34 -0300

    Adding template for MessageConfig.java

--- spring/spring-messaging/src/main/java/com/example/config/MessageConfig.java
+++ spring/spring-messaging/src/main/java/com/example/config/MessageConfig.java
@@ -1,1 +1,13 @@
-// TODO
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class MessageConfig {
+    // TODO
+}
+
