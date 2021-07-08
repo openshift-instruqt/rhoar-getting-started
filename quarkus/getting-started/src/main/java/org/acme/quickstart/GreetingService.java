d8ee6a6954a8c2c6a2d9095594c30e59baac3f42
Eric Deandrea <edeandrea@redhat.com>
2021-07-08 13:25:15 +0000

    Creating solution for getting-started scenario

---
+++ quarkus/getting-started/src/main/java/org/acme/quickstart/GreetingService.java
@@ -0,0 +1,14 @@
+package org.acme.quickstart;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    private String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+
+    public String greeting(String name) {
+        return "hello " + name + " from " + hostname;
+    }
+
+}
---
+++ quarkus/getting-started/src/test/java/org/acme/quickstart/GreetingServiceTest.java
@@ -0,0 +1,25 @@
+package org.acme.quickstart;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.startsWith;
+
+@QuarkusTest
+public class GreetingServiceTest {
+
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+          .pathParam("name", uuid)
+          .when().get("/hello/greeting/{name}")
+          .then()
+            .statusCode(200)
+            .body(startsWith("hello " + uuid));
+    }
+
+}
