6a3bc1612de6e053ecdbae2194917d2f25002b60
Eric Deandrea <edeandrea@redhat.com>
2021-07-09 17:06:24 +0000

    Spring rest services solution update

--- spring/spring-rest-services/src/main/java/com/example/service/FruitController.java
+++ spring/spring-rest-services/src/main/java/com/example/service/FruitController.java
@@ -11,27 +11,31 @@ public class FruitController {
 
     @Autowired FruitRepository repository; // Ignore this
 
-    @GetMapping
-    public List<Fruit> getAll() {
-        return repository.findAll();
-    }
+            @GetMapping
+        public List<Fruit> getAll() {
+            return repository.findAll();
+        }
 
-    @GetMapping("/{id}")
-    public Fruit getFruit(@PathVariable("id") Long id) {
-        return repository.findById(id).orElse(null);
-    }
-    @PostMapping
-    public Fruit createFruit(@RequestBody Fruit fruit) {
-        return repository.save(fruit);
-    }
-    @PutMapping("/{id}")
-    public Fruit updateFruit(@PathVariable("id") Long id, @RequestBody Fruit fruit) {
-        fruit.setId(id);
-        return repository.save(fruit);
-    }
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Long id) {
-        repository.deleteById(id);
-    }
+        @GetMapping("/{id}")
+        public Fruit getFruit(@PathVariable("id") Long id) {
+            return repository.findById(id).orElse(null);
+        }
+
+            @PostMapping
+        public Fruit createFruit(@RequestBody Fruit fruit) {
+            return repository.save(fruit);
+        }
+
+            @PutMapping("/{id}")
+        public Fruit updateFruit(@PathVariable("id") Long id, @RequestBody Fruit fruit) {
+            fruit.setId(id);
+            return repository.save(fruit);
+        }
+
+            @DeleteMapping("/{id}")
+        public void delete(@PathVariable("id") Long id) {
+            repository.deleteById(id);
+        }
+
 
 }
